448effe34aca2c0655696be241bedb2c81337719
Adam Pearson <adam.pearson.1996@gmail.com>
2020-05-18 14:35:46 +0100

    button/link hover states and general tidying of css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Glide, { Controls } from '@glidejs/glide/dist/glide.modular.esm'
 import "./main.scss";
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+  // Product Slider
   new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
